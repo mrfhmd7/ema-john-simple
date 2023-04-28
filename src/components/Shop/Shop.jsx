7700ad95a5029292e3dfd8352f7b14dab282dcc6
mrfhmd7 <mrf.hmd7@gmail.com>
2023-04-28 18:02:58 +0600

    shop file added

---
+++ src/components/Shop/Shop.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+     return (
+          <div className='shop-container'>
+               <div className='product-container'>
+                    <h2>Coming soon</h2>
+               </div>
+               <div className='cart-container'>
+                    <h4>Order Summary</h4>
+               </div>
+               
+          </div>
+     );
+};
+
+export default Shop;
