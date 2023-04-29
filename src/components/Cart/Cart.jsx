5d2a9d54a2a49341b9f04e2f5e9b78e0060b340b
mrfhmd7 <mrf.hmd7@gmail.com>
2023-04-29 16:58:39 +0600

    cart details added

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = ({ cart }) => {
+  // const cart = props.cart;
+  // const {cart} =props;
+//   console.log(cart);
+
+     let totalPrice = 0;
+     let shippingTotal = 0;
+  for (const product of cart) {
+       totalPrice = totalPrice + product.price;
+       shippingTotal = shippingTotal + product.shipping;
+     }
+     
+     const tax = totalPrice * 3 / 100;
+     const grandTotal = totalPrice + shippingTotal + tax;
+
+  return (
+    <div className="cart">
+          <h4>Order Summary</h4>
+          <p>Selected item: {cart.length}</p>
+          <p>Total Price: ${totalPrice}</p>
+          <p>Total Shipping Charge: ${shippingTotal}</p>
+          <p>Tax: ${tax.toFixed(2)}</p>
+          <h5>Grand Total: ${grandTotal.toFixed(2)} </h5>
+    </div>
+  );
+};
+
+export default Cart;
