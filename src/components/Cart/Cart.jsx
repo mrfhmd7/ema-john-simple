1f6ac254c491e811f98b80c2cd60c83916c7fa92
mrfhmd7 <mrf.hmd7@gmail.com>
2023-04-29 22:12:57 +0600

    get product details from local storage and calculate with newly added product

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,29 +1,38 @@
-import React from 'react';
-import './Cart.css'
+import React from "react";
+import "./Cart.css";
 
 const Cart = ({ cart }) => {
   // const cart = props.cart;
   // const {cart} =props;
-    console.log(cart);
+  // console.log(cart);
 
-     let totalPrice = 0;
-     let shippingTotal = 0;
+  let totalPrice = 0;
+  let shippingTotal = 0;
+  let quantity = 0;
+
+  //  if(product.quantity === 0){
+  //   product.quantity = 1;
+  //  }
+
+  //product.quantity = product.quantity || 1;
+
   for (const product of cart) {
-       totalPrice = totalPrice + product.price;
-       shippingTotal = shippingTotal + product.shipping;
-     }
-     
-     const tax = totalPrice * 3 / 100;
-     const grandTotal = totalPrice + shippingTotal + tax;
+    totalPrice = totalPrice + product.price * product.quantity;
+    shippingTotal = shippingTotal + product.shipping * product.quantity;
+    quantity = quantity + product.quantity;
+  }
+
+  const tax = (totalPrice * 3) / 100;
+  const grandTotal = totalPrice + shippingTotal + tax;
 
   return (
     <div className="cart">
-          <h4>Order Summary</h4>
-          <p>Selected item: {cart.length}</p>
-          <p>Total Price: ${totalPrice}</p>
-          <p>Total Shipping Charge: ${shippingTotal}</p>
-          <p>Tax: ${tax.toFixed(2)}</p>
-          <h5>Grand Total: ${grandTotal.toFixed(2)} </h5>
+      <h4>Order Summary</h4>
+      <p>Selected item: {quantity}</p>
+      <p>Total Price: ${totalPrice}</p>
+      <p>Total Shipping Charge: ${shippingTotal}</p>
+      <p>Tax: ${tax.toFixed(2)}</p>
+      <h5>Grand Total: ${grandTotal.toFixed(2)} </h5>
     </div>
   );
 };
