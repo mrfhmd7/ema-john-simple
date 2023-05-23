ad5df53feb903eb0d9ab3fa6a8898d14585c902d
mrfhmd7 <mrf.hmd7@gmail.com>
2023-05-24 00:45:40 +0600

    Home and Orders page added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,9 +7,9 @@ const Header = () => {
        <nav className="header">
            <img src={logo} alt="" />
          <div >
-           <a href="/order">Order</a>
-           <a href="/order-review">Order Review</a>
-           <a href="/manage-inventory">Manage Inventory</a>
+           <a href="/">Shop</a>
+           <a href="/orders">Orders</a>
+           <a href="/inventory">Manage Inventory</a>
            <a href="/login">Login</a>
          </div>
        </nav>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Layout/Home.jsx";
+import Shop from "./components/Shop/Shop.jsx";
+import Orders from "./components/Orders/Orders.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+    children: [
+      {
+        path: "/",
+        element: <Shop></Shop>,
+      },
+      {
+        path: "orders",
+        element: <Orders></Orders>
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
