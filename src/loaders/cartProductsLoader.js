08aa06f43c6702e1d44d3ac7aa9f01189566fe2f
mrfhmd7 <mrf.hmd7@gmail.com>
2023-05-25 00:10:26 +0600

    added cart in order page

--- src/loaders/cartProductsLoader.js
+++ src/loaders/cartProductsLoader.js
@@ -1,7 +1,30 @@
+import { getShoppingCart } from "../utilities/fakedb";
+
 const cartProductLoader = async() => {
      const loadedProduct = await fetch('products.json');
      const products = await loadedProduct.json();
-     console.log(products);
+
+     //if cart data in database, you have to use async await
+
+     const storedCart = getShoppingCart();
+     const savedCart = [];
+
+     console.log(storedCart);
+
+     for (const id in storedCart) {
+          const addedProduct = products.find(pd => pd.id === id);
+          if (addedProduct) {
+               const quantity = storedCart[id];
+               addedProduct.quantity = quantity;
+               savedCart.push(addedProduct);
+          }
+     }
+
+     //if you need to send two things
+     //return [products, savedCart]
+     //another options
+     //return { products, cart: savedCart }
+     return savedCart;
 }
 
 export default cartProductLoader;
