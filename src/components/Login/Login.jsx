2959ab53f6687b4b80e91cd1fed5223dcc61cc54
mrfhmd7 <mrf.hmd7@gmail.com>
2024-08-26 23:34:36 +0600

    login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,8 +3,18 @@ import './Login.css'
 
 const Login = () => {
      return (
-          <div>
-               <h2>Login coming soon....</h2>
+          <div className='form-container'>
+               <h2 className="form-title">Login</h2>
+               <form>
+                    <div className="form-control">
+                         <label htmlFor="email">Email</label>
+                         <input type="email" name="email" id="" required />
+                    </div>
+                    <div className="form-control">
+                         <label htmlFor="password">Password</label>
+                         <input type="password" name="password" id="" required />
+                    </div>
+               </form>
           </div>
      );
 };
